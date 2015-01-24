380246dd1292103fe79046bf632929ea0c48df46
val_pinkman <design.valentin@gmail.com>
2015-01-24 23:21:05 +0100

    pokemon model correction

--- app/models/pokemon.js
+++ app/models/pokemon.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var PokemonSchema = new Schema({
+var PokeSchema = new Schema({
     name: String,
     id: Number,
     types: []
