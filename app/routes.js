9fc04afff23b35b850851bb78219135e82d4a34a
val_pinkman <design.valentin@gmail.com>
2015-01-25 00:08:04 +0100

    pokeapi module added. Backend for /pokemons and /pokemon/:id ready

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 // grab the pokemon model
 var Pokemon = require('./models/pokemon');
+var dex = require('node-pokeapi')();
 
 module.exports = function(app) {
 
@@ -7,15 +8,21 @@ module.exports = function(app) {
     // handle things like api calls
     // authentication routes
 
-    // sample api route
-    app.get('/api/pokemon/:id', function(req, res) {
+    // get all pokemons for index page
+    app.get('/api/pokemons', function(req, res) {
 
         Pokemon.find(function(err, pokemons) {
-
             if(err)
                 res.send(err);
 
             res.json(pokemons);
+        });
+    });
+
+    // get a pokemon based on its national pokedex ID
+    app.get('/api/pokemon/:id', function(req, res) {
+        var pokemon = dex.get({cat: 'pokemon', id: req.params.id }, function(err, response, pokemon) {
+            res.json(pokemon);
         });
     });
 
