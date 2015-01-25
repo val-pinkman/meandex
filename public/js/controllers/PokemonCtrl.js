8d0c5d6bea3ce8ce97bb81af6925a47e8a89416f
val_pinkman <design.valentin@gmail.com>
2015-01-26 00:26:20 +0100

    first integration of data into pokemon/:id template

--- public/js/controllers/PokemonCtrl.js
+++ public/js/controllers/PokemonCtrl.js
@@ -1,5 +1,12 @@
-angular.module('PokemonCtrl', []).controller('PokemonController', function($scope) {
+angular.module('PokemonCtrl', []).controller('PokemonController', function($scope, $location, $routeParams, Pokemon) {
 
-    $scope.tagline = "Pokemon Controller Bitch";
+    $scope.pokeId = $routeParams.pokeId;
+    $scope.pokeUrl = 'img/pokemons/pokemon' + $scope.pokeId + '.jpg';
+
+    Pokemon.getPokemon($scope.pokeId).success(function(poke) {
+        $scope.poke = poke;
+        $('.parallax').parallax();
+        console.log(poke);
+    });
 
 });
