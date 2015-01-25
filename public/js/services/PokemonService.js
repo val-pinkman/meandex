efde540915fda9fad22e6759a4a61eca0b9e60a4
val_pinkman <design.valentin@gmail.com>
2015-01-25 03:07:05 +0100

    api routes front continued

--- public/js/services/PokemonService.js
+++ public/js/services/PokemonService.js
@@ -1,19 +1,17 @@
 angular.module('PokemonService', []).factory('Pokemon', ['$http', function($http) {
 
     return {
-        get: function(i) {
+
+        getPokemon: function(i) {
             return $http.get('/api/pokemon/' + i);
         },
+
         getAll: function($scope) {
-            return $http.get('/api/pokemons')
-                .success(function(response) {
-                    console.log(response);
-                })
-                .error(function(err) {
+            return $http.get('/api/pokemons').error(function(err) {
                     console.log(err);
                 });
         }
 
-
     }
-}])
+
+}]);
