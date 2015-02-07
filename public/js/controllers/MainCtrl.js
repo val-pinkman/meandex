52c8f7d81e85772b98e1ce0a2d67a1dc69ac842b
val_pinkman <design.valentin@gmail.com>
2015-02-07 19:33:54 +0100

    controller to check for width of document to toggle container and card classes

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,4 +1,17 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope, $location, Pokemon) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $location, Pokemon, $document, $window) {
+
+    $document.ready(function () {
+        $scope.winWidth = $window.outerWidth;
+        $scope.containerClass = $scope.winWidth > 600 ? 'container' : '';
+        $scope.cardClass = $scope.winWidth > 600 ? 'card' : '';
+    });
+
+    $(window).on('resize', function () {
+        $scope.winWidth = $window.outerWidth;
+        $scope.containerClass = $scope.winWidth > 600 ? 'container' : '';
+        $scope.cardClass = $scope.winWidth > 600 ? 'card' : '';
+    });
+
 
     Pokemon.getAll().success(function(pokemons) {
         pokemons.forEach(function(pokemon) {
