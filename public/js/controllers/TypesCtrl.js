5ef6eaa9330c7debce42725e49eca6256d144c9b
Maxime Charpentier <maxime.charpentier@fullsix.com>
2015-02-25 18:09:30 +0100

    C:/Program Files (x86)/Git/types WIP(ekino)

--- public/js/controllers/TypesCtrl.js
+++ public/js/controllers/TypesCtrl.js
@@ -1,12 +1,12 @@
 angular.module('TypesCtrl', []).controller('TypesController', function($scope, $location, $stateParams, Pokemon, $q, $document) {
 
-  var types = {};
+  $scope.types = {};
 
   for (i=1; i <= 18; i++) {
     Pokemon.getType(i).success(function(data) {
       //console.log(data);
-      types[data.name.toLowerCase()] = data;
-      console.log(types);
+      $scope.types[data.name.toLowerCase()] = data;//[data.name.toLowerCase()]
+      console.log($scope.types);
     });
   };
 
@@ -14,15 +14,15 @@ angular.module('TypesCtrl', []).controller('TypesController', function($scope, $
     //check if no nav animation is ongoing
     if( !is_bouncy_nav_animating) {
       is_bouncy_nav_animating = true;
-      
+
       //toggle list items animation
       $('.cd-bouncy-nav-modal').toggleClass('fade-in', $bool).toggleClass('fade-out', !$bool).find('li:last-child').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
         $('.cd-bouncy-nav-modal').toggleClass('is-visible', $bool);
         if(!$bool) $('.cd-bouncy-nav-modal').removeClass('fade-out');
         is_bouncy_nav_animating = false;
       });
-      
-      //check if CSS animations are supported... 
+
+      //check if CSS animations are supported...
       if($('.cd-bouncy-nav-trigger').parents('.no-csstransitions').length > 0 ) {
         $('.cd-bouncy-nav-modal').toggleClass('is-visible', $bool);
         is_bouncy_nav_animating = false;
