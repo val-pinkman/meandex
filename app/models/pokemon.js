c4a426bc48973653d31696192c07ff6c1f51c134
val_pinkman <design.valentin@gmail.com>
2015-03-02 15:16:50 +0100

    modifications de la bdd

--- app/models/pokemon.js
+++ app/models/pokemon.js
@@ -5,7 +5,29 @@ var Schema = mongoose.Schema;
 var PokeSchema = new Schema({
     name: String,
     id: Number,
-    types: []
+    types: [{name: String}],
+    abilities: [{name: String}],
+    attack: Number,
+    catch_rate: Number,
+    defense: Number,
+    descriptions: [{name: String, resource_uri: String}],
+    egg_cycles: Number,
+    egg_groups: [{name: String, resource_uri: String}],
+    ev_yield: String,
+    evolutions: [],
+    exp: Number,
+    growth_rate: String,
+    happiness: Number,
+    height: String,
+    hp: Number,
+    male_female_ratio: String,
+    moves: [],
+    sp_atk: Number,
+    sp_def: Number,
+    species: String,
+    speed: Number,
+    total: Number,
+    weight: String
 });
 
 // define our pokemon model
