82c34317cccba706b37da7815699beed040fec70
val_pinkman <design.valentin@gmail.com>
2015-03-25 12:27:59 +0100

    autoprefixr css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     connect = require('gulp-connect'),
+    autoprefixer = require('gulp-autoprefixer');
     lr = require('tiny-lr')();
 
 gulp.task('connect', function() {
@@ -14,26 +15,25 @@ gulp.task('connect', function() {
 // by `gulp.watch()`
 function notifyLivereload(event) {
 
-  // `gulp.watch()` events provide an absolute path
-  // so we need to make it relative to the server root
-  var fileName = require('path').relative(__dirname, event.path);
-
-  lr.changed({
-    body: {
-      files: [fileName]
-    }
-  });
+    gulp.src(event.path, {read:false})
+        .pipe(require('gulp-livereload')(lr));
 }
 
 gulp.task('sass', function() {
     return sass('./public/scss/')
     .on('error', function(err) { console.log(err.message); })
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
     .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('watch', function() {
     gulp.watch(['./public/scss/**/*.*'], ['sass']);
     gulp.watch('./public/**/*.*', notifyLivereload);
+    gulp.watch('./server.js', notifyLivereload);
+    gulp.watch('./app/**/*.*', notifyLivereload);
+    gulp.watch('./config/*.*', notifyLivereload);
 });
 
-gulp.task('default', ['connect', 'watch', 'sass']);
+gulp.task('default', ['connect', 'sass', 'watch']);
